4fa8084393180943c60790cf569aad697da3272b
Michal Altair Valášek <michal.valasek@altairis.cz>
2013-10-03 00:27:39 +0200

    Added ProxyRouteHandler class

---
+++ Altairis.BlobCdn/ProxyRouteHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace Altairis.BlobCdn {
+    public class ProxyRouteHandler : IRouteHandler {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext) {
+            return new ProxyHandler();
+        }
+    }
+}
